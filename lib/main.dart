09455724d133176136e652c26fb1a0a74a3b72af
Amr Ezz <amrezz0111@gmail.com>
2020-10-11 10:06:55 +0200

    App initiation

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('New App'),
+        ),
+        body: Center(
+          child: Text('Center Text'),
+        ),
+      ),
+    );
+  }
+}
